afcfba8dd8042d60444fc8253719c3b6fd803c93
Christian Battaglia <christian.d.battaglia@gmail.com>
2026-03-01 16:41:19 -0500

    chore: v0.2.4 release prep
    Bump _VERSION to 0.2.4 across all modules and add LuaRocks publish
    workflow triggered on GitHub Release. The workflow generates a versioned
    rockspec from lua-resty-jwt-dev-0.rockspec via luarocks new_version.

--- lib/resty/jwt-validators.lua
+++ lib/resty/jwt-validators.lua
@@ -1,4 +1,4 @@
-local _M = { _VERSION = "0.2.3" }
+local _M = { _VERSION = "0.2.4" }
 
 --[[
   This file defines "validators" to be used in validating a spec.  A "validator" is simply a function with
